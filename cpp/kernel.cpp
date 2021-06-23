538332f2855fc14a20b4430c33988935401c3358
vmlinuz719 <dlevine.levine@gmail.com>
2021-06-22 23:19:10 -0400

    Added bitmap allocator, also some build/run utils

--- cpp/kernel.cpp
+++ cpp/kernel.cpp
@@ -1,33 +1,3 @@
-/*
- * mykernel/cpp/kernel.cpp
- *
- * Copyright (C) 2017 - 2021 bzt (bztsrc@gitlab)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * This file is part of the BOOTBOOT Protocol package.
- * @brief A sample BOOTBOOT compatible kernel
- *
- */
-
 #include <stdint.h>
 #include <bootboot.h>
 #include <heap.h>
@@ -39,6 +9,7 @@
 #include <gdt.h>
 #include <ksync.h>
 #include <cpuid.h>
+#include <bitmap.h>
 
 char *int2Hex(uint64_t x, char *buf, size_t size, int radix) {
 	// expect buf to be n characters long - last one gets \0
@@ -85,13 +56,13 @@ SegmentDescriptor gdt[8];
 
 extern G2BootConsole console;
 
+Bitmap bmp((uint8_t *) 0, 0);
+
 class G2Kernel {
 
 public:
     
     G2Kernel() {
-        /*** NOTE: this code runs on all cores in parallel ***/
-        
         int x, y, s=bootboot.fb_scanline;
 
         if(s) {
@@ -123,6 +94,9 @@ public:
         console.puts("\n");
 
         MMapEnt *mmap_ent = &bootboot.mmap;
+        uint64_t maxptr = 0;
+        size_t maxsize = 0;
+
         for (unsigned int i = 0; i < (bootboot.size - 128) / 16; i++) {
             uint64_t ptr = (mmap_ent + i)->ptr;
             uint64_t size = (mmap_ent + i)->size;
@@ -149,6 +123,10 @@ public:
                     break;
                 case MMAP_FREE:
                     console.puts("FREE");
+                    if (actualSize > maxsize) {
+                        maxsize = actualSize;
+                        maxptr = ptr;
+                    }
                     break;
                 case MMAP_ACPI:
                     console.puts("ACPI");
@@ -160,6 +138,27 @@ public:
 
             console.puts("\n");
         }
+
+        char *sz = int2Hex(maxsize / 4096, buf, sizeof(buf), 10);
+        console.puts("BMP: Using ");
+        console.puts(sz);
+        int2Hex(maxptr, buf, sizeof(buf), 16);
+        console.puts(" physical blocks at 0x");
+        console.puts(buf);
+        console.puts("\n");
+
+        bmp = Bitmap((uint8_t *) maxptr, (size_t) (maxsize / 4096));
+
+        Allocation alloc1 = bmp.allocate(1);
+
+        sz = int2Hex(alloc1.numBlocks, buf, sizeof(buf), 10);
+        console.puts("Allocated ");
+        console.puts(sz);
+        int2Hex((uint64_t) alloc1.offset, buf, sizeof(buf), 16);
+        console.puts(" physical blocks at 0x");
+        console.puts(buf);
+        console.puts("\n");
+
      }
     
     void hang(void) {
